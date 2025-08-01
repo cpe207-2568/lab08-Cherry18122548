f7934d5c2e86f26908757b3e7f8c1b465a16993b
Cherry18122548 <Cherryworanat@gmail.com>
2025-08-02 02:01:12 +0700

    lab08

---
+++ src/components/TaskInput.tsx
@@ -0,0 +1,20 @@
+//TaskInput
+export function TaskInput() {
+  return (
+    <div className="m-2 p-2">
+      <form className="row">
+        <h2>Woranat Thammawit</h2>
+        <div className="col-10">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Insert a task here.."
+          ></input>
+        </div>
+        <div className="col-2">
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </form>
+    </div>
+  );
+}
